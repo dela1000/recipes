1ec13226cdaa7140f85165b09d2ffcceb6d3cfdf
Daniel De La Rosa <dela1000@gmail.com>
2021-09-24 18:03:29 +0200

    add helper files

---
+++ src/helpers/measurementCollection.js
@@ -0,0 +1,17 @@
+const measurements = {
+  ounce: 'ounce',
+  gram: 'gram',
+  pound: 'pound',
+  kilogram: 'kilogram',
+  pinch: 'pinch',
+  liter: 'liter',
+  'fluid ounce': 'fluid ounce',
+  gallon: 'gallon',
+  pint: 'pint',
+  quart: 'quart',
+  milliliter: 'milliliter',
+  drop: 'drop',
+  cup: 'cup',
+  tablespoon: 'tablespoon',
+  reaspoon: 'reaspoon',
+};
