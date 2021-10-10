803d8d355ff828203bfab32687f118c3cb42ff93
Daniel De La Rosa <dela1000@gmail.com>
2021-10-10 23:38:57 +0100

    add manualRecipe to recipeList

--- src/components/AddToShoppingListInput/AddToShoppingListInput.jsx
+++ src/components/AddToShoppingListInput/AddToShoppingListInput.jsx
@@ -1,29 +1,76 @@
 import { useState } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Input from '@material-ui/core/Input';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useRecoilState } from 'recoil';
 
-import { allRecipesState } from '../../contexts/atoms/atoms';
+import {
+  dbState,
+  currentUserState,
+  allRecipesState,
+  onShoppingListState,
+} from '../../contexts/atoms/atoms';
+import { addRecipe, updateRecipe } from '../../adapters/recipeAdapters';
 
 export default function AddToShoppingListInput() {
   const [filterText, setFilterText] = useState('');
-  const recipesData = useRecoilValue(allRecipesState);
-  // const db = useRecoilValue(dbState);
-  // const currentUser = useRecoilValue(currentUserState);
+  const [allRecipes, setAllRecipes] = useRecoilState(allRecipesState);
+  const [recipesOnShoppingList, setRecipesOnShoppingList] = useRecoilState(onShoppingListState);
+  const db = useRecoilValue(dbState);
+  const currentUser = useRecoilValue(currentUserState);
   const handleItemInput = (event) => {
     setFilterText(event.target.value);
   };
 
-  const handleKeyDown = (e) => {
+  const handleKeyDown = async (e) => {
     if (e.key === 'Enter') {
-      console.log(
-        '+++ 17: src/components/AddToShoppingListInput/AddToShoppingListInput.jsx - filterText: ',
-        filterText,
-      );
-      console.log(
-        '+++ 23: src/components/AddToShoppingListInput/AddToShoppingListInput.jsx - recipesData: ',
-        recipesData,
-      );
+      let docRef;
+      const manualListFound = allRecipes.filter((recipe) => recipe.manualList === true);
+      const newItem = {
+        purchased: false,
+        string: filterText,
+      };
+      const parsedAllRecipes = JSON.parse(JSON.stringify(allRecipes));
+      const newRecipesOnShoppingList = JSON.parse(JSON.stringify(recipesOnShoppingList));
+
+      if (!manualListFound[0]) {
+        newItem.index = 0;
+        const manualRecipeObj = {
+          title: 'Manual Items',
+          ingredients: [
+            {
+              index: 0,
+              ingredients: [newItem],
+              groupName: 'standard',
+            },
+          ],
+          onShoppingList: true,
+          deleted: false,
+          manualList: true,
+        };
+        docRef = await addRecipe({
+          db,
+          currentUserId: currentUser.uid,
+          payload: manualRecipeObj,
+        });
+        newRecipesOnShoppingList.push(docRef);
+        parsedAllRecipes.push(docRef);
+      } else {
+        const newManualList = JSON.parse(JSON.stringify(manualListFound));
+        newItem.index = newManualList[0].ingredients[0].ingredients[0].length - 1;
+        newManualList[0].ingredients[0].ingredients.push(newItem);
+        docRef = await updateRecipe({
+          db,
+          currentUserId: currentUser.uid,
+          recipeId: newManualList[0].id,
+          payload: newManualList[0],
+        });
+        const foundIndex = newRecipesOnShoppingList.findIndex((x) => x.id === docRef.id);
+        [newRecipesOnShoppingList[foundIndex]] = [newManualList[0]];
+        const foundAllIndex = parsedAllRecipes.findIndex((x) => x.id === docRef.id);
+        [parsedAllRecipes[foundAllIndex]] = [newManualList[0]];
+      }
+      setRecipesOnShoppingList(newRecipesOnShoppingList);
+      setAllRecipes(parsedAllRecipes);
       setFilterText('');
     }
   };
